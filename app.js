541f89c7284de66b65dc59efe617df7349e306a2
Alaa Aldajan <150370395+Alaa-Aldajan@users.noreply.github.com>
2024-01-02 17:15:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-(function() {
-    'use strict';
+(function () {
+'use strict';
 
-    angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController);
+angular.module('LunchChecker', [])
 
-    LunchCheckController.$inject = ['$scope'];
+.controller('LunchCheckerCtrl', function ($scope) {
+  $scope.message = "";
+  $scope.dishes = "";
+  $scope.checkIfTooMuch = function () {
+      if($scope.dishes == "") {
+          $scope.message = "Please enter data first";
+      } else {
+          var items = itemsInText($scope.dishes);
+          if(items == 0 || $scope.dishes == "") {
+              $scope.message = "Empty";
+          } else if(items <= 3) {
+              $scope.message = "Enjoy!";
+          } else {
+              $scope.message = "Too much!";
+          }
+      }
 
-    function LunchCheckController($scope) {
-        $scope.dishes = '';
-        $scope.message = '';
-        $scope.checked = false;
+  };
 
-        $scope.checkLunch = function() {
-            if ($scope.dishes.trim().length === 0) {
-                $scope.empty = true;
-            } else {
-                $scope.checked = true;
-                $scope.empty = false;
-                var arrayDishes = $scope.dishes.split(',');
-                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
-                    return v.trim() !== '';
-                });
 
-                if (arrayDishesWithoutEmptys.length <= 3) {
-                    $scope.message = 'Enjoy!';
-                } else {
-                    $scope.message = 'Too much!';
-                }
-            }
-        };
-    }
+  function itemsInText(string) {
+    var arrayOfStrings = string.split(",");
+    console.log(arrayOfStrings);
+    return arrayOfStrings.length;
+  }
+
+});
+
+
 })();
